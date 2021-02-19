ffea2d1f5335d3a76c64b0ad1188793dddfd12d8
Dayane Damaceno <dayane.damaceno@hotmail.com>
2021-02-19 14:44:40 -0300

    Exe 02 Concluido - Métodos - Par ou Impar

---
+++ Exe_02_ParOuImpar/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exe_02_ParOuImpar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+
+            Console.Write("Insira um número: ");
+            numero = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(ParOuImpar(numero));
+
+            Console.ReadKey();
+        }
+
+        static string ParOuImpar(int numero)
+        {
+            if (numero % 2 == 0)
+                return "Par";
+            else
+                return "Impar";
+        }
+    }
+}
