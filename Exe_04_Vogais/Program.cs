50f092b68a155a72517d753d0f3ac137c2baab99
Dayane Damaceno <dayane.damaceno@hotmail.com>
2021-02-19 15:27:31 -0300

    Exe04 Concluido - Métodos - Vogais

---
+++ Exe_04_Vogais/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exe_04_Vogais
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome;
+
+            Console.WriteLine("Insira um nome: ");
+            nome = Console.ReadLine();
+
+            ReadVogais(nome);
+
+            Console.ReadKey();
+        }
+
+        static void ReadVogais(string nome)
+        {
+            for (int i = 0; i < nome.Length; i++)
+            {
+                if ("AEIOUaeiou".IndexOf(nome[i]) != -1)
+                {
+                    Console.Write($"{nome[i]} - ");
+                }
+            }
+        }
+    }
+}
