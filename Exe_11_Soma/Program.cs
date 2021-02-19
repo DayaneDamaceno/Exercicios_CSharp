e1a710abef84b205aaeeccba3a3d498665ff37fc
Dayane Damaceno <dayane.damaceno@hotmail.com>
2021-02-19 16:09:35 -0300

    Exe11 Concluido - Métodos - Somatoria

---
+++ Exe_11_Soma/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exe_11_Soma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numeros = new int[5];
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write($"Insira o {i + 1}º número: ");
+                numeros[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.WriteLine(Somatoria(numeros));
+
+            Console.ReadKey();
+
+        }
+
+        static int Somatoria(int[] numeros)
+        {
+            int soma = 0;
+
+            for (int i = 0; i < 5; i++)
+            {
+                soma += numeros[i];
+            }
+
+            return soma;
+        }
+    }
+}
