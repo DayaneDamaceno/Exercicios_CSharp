90f78fc849cbd80abdc7ad2a9ee64a6e47629c92
Dayane Damaceno <dayane.damaceno@hotmail.com>
2021-02-19 15:52:24 -0300

    Exe07 Concluido - Métodos - Verificar espaços

---
+++ Exe_07_VerificaSpace/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exe_07_VerificaSpace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string texto;
+
+            Console.WriteLine("Insira um texto:");
+            texto = Console.ReadLine();
+
+            if (VerificaSpace(texto))
+                Console.WriteLine("O seu texto tem espaços");
+            else
+                Console.WriteLine("O seu texto não tem espaços");
+
+
+            Console.ReadKey();
+
+        }
+
+        static bool VerificaSpace(string texto)
+        {
+            if (texto.IndexOf(" ") != -1)
+                return true;
+            else
+                return false;
+        }
+    }
+}
