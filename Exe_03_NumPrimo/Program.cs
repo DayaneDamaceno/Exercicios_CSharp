d415168d989fb313988dcdb092b8abb4080480da
Dayane Damaceno <dayane.damaceno@hotmail.com>
2021-02-19 15:08:43 -0300

    Exe03 Concluido - Métodos - Número Primo

---
+++ Exe_03_NumPrimo/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exe_03_NumPrimo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+
+            Console.Write("Insira um número: ");
+            numero = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(VerificaPrimo(numero));
+
+            Console.ReadKey();
+        }
+
+        static string VerificaPrimo(int numero)
+        {
+ 
+            int divisores = 0;
+
+            for (int i = 1; i <= numero; i++)
+            {
+                if (numero % i == 0)
+                    divisores++;  
+            }
+
+            if (divisores == 2) return $"{numero} é primo.";
+            else return $"{numero} não é primo.";
+
+        }
+    }
+}
