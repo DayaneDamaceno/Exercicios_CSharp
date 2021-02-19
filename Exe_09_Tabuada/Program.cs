4e0211e9eea5aab2417ec3391ff295938434fc5e
Dayane Damaceno <dayane.damaceno@hotmail.com>
2021-02-19 15:58:26 -0300

    Exe09 Concluido - Métodos - Tabuada

---
+++ Exe_09_Tabuada/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exe_09_Tabuada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+
+            Console.Write("Insira um número: ");
+            numero = Convert.ToInt32(Console.ReadLine());
+
+            ReadTabuada(numero);
+
+            Console.ReadKey();
+        }
+
+        static void ReadTabuada(int numero)
+        {
+            for (int i = 0; i <= 10; i++)
+            {
+                Console.WriteLine($"{i} x {numero} = {i*numero}");
+            }
+        }
+    }
+}
