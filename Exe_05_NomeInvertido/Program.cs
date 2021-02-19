2d4d865a8669a38ae54fd3ed6637c46c14b8313f
Dayane Damaceno <dayane.damaceno@hotmail.com>
2021-02-19 15:44:57 -0300

    Exe05 Concluido - Métodos - Nome Invertido

---
+++ Exe_05_NomeInvertido/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exe_05_NomeInvertido
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string texto;
+
+            Console.WriteLine("Insira um texto:");
+            texto = Console.ReadLine();
+
+            Console.WriteLine($"Texto Invertido - {TextoInvertido(texto)}");
+
+            Console.ReadKey();
+        }
+
+        static string TextoInvertido(string texto)
+        {
+            string textoInvertido = "";
+
+            for (int i = texto.Length - 1; i >= 0; i--)
+            {
+                textoInvertido += texto[i];
+            }
+
+            return textoInvertido;
+        }
+    }
+}
