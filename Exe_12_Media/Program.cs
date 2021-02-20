76c2eb89c689423de1fd86c4c6e7184aac1bb4da
Dayane Damaceno <dayane.damaceno@hotmail.com>
2021-02-19 23:27:45 -0300

    Exe12 Concluido - Métodos - Média

---
+++ Exe_12_Media/Program.cs
@@ -0,0 +1,40 @@
+﻿    using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exe_12_Media
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numeros = new int[3];
+
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                Console.WriteLine($"Insira o {i + 1}º número: ");
+                numeros[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.WriteLine($"O média dos números é - {Media(numeros)}");
+
+            Console.ReadKey();
+        }
+
+        static int Media(int[] numeros)
+        {
+            int media = 0, soma = 0;
+
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                soma += numeros[i];
+            }
+
+            media = soma / numeros.Length;
+
+            return media;
+        }
+    }
+}
