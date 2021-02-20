58fdde774a347f2dfffbd070bb75f7312fcb4eb1
Dayane Damaceno <dayane.damaceno@hotmail.com>
2021-02-19 23:10:34 -0300

    Exe08 Concluido - Métodos - Validar E-mail

---
+++ Exe_08_ValidarEmail/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exe_08_ValidarEmail
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string email;
+
+            Console.WriteLine("Insira seu e-mail: ");
+            email = Console.ReadLine();
+
+            if (VerificaEmail(email))
+                Console.WriteLine("E-mail é valido!");
+            else
+                Console.WriteLine("E-mail é invalido!");
+
+            Console.ReadKey();
+        }
+
+        static bool VerificaEmail(string email)
+        {
+            int arroba = email.IndexOf('@');
+            int ponto = email.IndexOf('.');
+
+            if (arroba != -1 && ponto != -1)
+            {
+                if((arroba == 0) || (ponto == 0) || (arroba == email.Length - 1) || (ponto == email.Length - 1))
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+    }
+}
