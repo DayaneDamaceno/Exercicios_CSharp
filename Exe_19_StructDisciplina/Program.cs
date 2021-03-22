02729cf0f840c7fa05983a6636df612a30bbad94
Dayane Damaceno <dayane.damaceno@hotmail.com>
2021-03-22 17:24:24 -0300

    Exe 19 e 20 Concluidos - Trabalhando com struct

---
+++ Exe_19_StructDisciplina/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exe_19_StructDisciplina
+{
+    class Program
+    {
+        /*
+         19. Faça um programa para gravar os seguintes dados de uma disciplina:
+            nome : string; Não pode estar vazio
+            nota de aprovação : int ; Entre 1 e 10
+            Ao final exiba os dados no formato nome – nota de aprovação 
+         */
+
+        struct Disciplina
+        {
+            public string nome;
+            public int notaAprovacao;
+        }
+
+        static void Main(string[] args)
+        {
+            Disciplina disciplina1 = new Disciplina();
+
+            do
+            {
+                Console.Write("Insira o nome da disciplina: ");
+                disciplina1.nome = Console.ReadLine();
+
+            } while (disciplina1.nome == "");
+
+            do
+            {
+                try
+                {
+                    Console.Write($"Insira a nota de aprovação para {disciplina1.nome}: ");
+                    disciplina1.notaAprovacao = Convert.ToInt32(Console.ReadLine());
+                }
+                catch
+                {
+                    Console.WriteLine("Insira apenas números de 1 a 10");
+                }
+
+            } while (disciplina1.notaAprovacao <= 0 || disciplina1.notaAprovacao > 10);
+
+
+            Console.WriteLine(disciplina1.nome);
+            Console.WriteLine(disciplina1.notaAprovacao);
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Exe_20_SistemaAcademico/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exe_20_SistemaAcademico
+{
+    class Program
+    {
+        /*
+         20. Sistema Acadêmico
+            Faça um programa que solicite os seguintes dados de um aluno: nome, telefone, idade, valor da mensalidade.
+            Ao final do programa, exiba os dados cadastrados.
+            Validações:
+            Nome deve ser preenchido, telefone deve ter exatamente 9 dígitos (contando o “-“)
+            O primeiro dígito do celular não pode iniciar com 7,8 ou 9
+            Idade deve estar entre 0 e 150 
+            mensalidade deve ser >= 0.
+
+         */
+
+        struct Aluno
+        {
+            public string nome;
+            public string telefone;
+            public int idade;
+            public double valorMensalidade;
+        }
+
+        static void Main(string[] args)
+        {
+            Aluno aluno1 = new Aluno();
+            bool repeat = false;
+
+            do
+            {
+                Console.Write("Insira o nome do aluno: ");
+                aluno1.nome = Console.ReadLine();
+
+            } while (aluno1.nome == "");
+
+            do
+            {
+                repeat = false;
+                Console.Write("Insira o telefone do aluno: ");
+                aluno1.telefone = Console.ReadLine();
+
+                if ("789".IndexOf(aluno1.telefone[0]) != -1)
+                {
+                    Console.WriteLine("Não é valido telefone que comece com 7,8 ou 9");
+                    repeat = true;
+                }
+
+            } while (aluno1.telefone.Length != 9 || repeat);
+
+            do
+            {
+                try
+                {
+                    Console.Write("Insira a idade do aluno: ");
+                    aluno1.idade = Convert.ToInt32(Console.ReadLine());
+                }
+                catch
+                {
+                    Console.WriteLine("Insira apenas idades de 1 a 150");
+                }
+
+            } while (aluno1.idade < 0 || aluno1.idade >= 150);
+
+            do
+            {
+                try
+                {
+                    Console.Write("Insira o valor da mensalidade: ");
+                    aluno1.valorMensalidade = Convert.ToDouble(Console.ReadLine());
+                }
+                catch
+                {
+                    Console.WriteLine("Insira apenas números maiores que 0");
+                }
+
+            } while (aluno1.valorMensalidade < 0);
+
+
+            Console.WriteLine(aluno1.nome);
+            Console.WriteLine(aluno1.telefone);
+            Console.WriteLine(aluno1.idade);
+            Console.WriteLine(aluno1.valorMensalidade);
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
